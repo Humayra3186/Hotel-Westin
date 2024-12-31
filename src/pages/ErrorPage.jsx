e662b1504676ee30620e44246c3c66b12ac91958
Humayra3186 <humayra.personal@gmail.com>
2024-12-31 12:41:42 +0600

    error page is done

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -3,16 +3,23 @@ import { Link } from 'react-router-dom';
 
 const ErrorPage = () => {
     return (
-        <div className='flex justify-center items-center min-h-screen w-full'>
-        <div className='text-center'>
-            <h1 className='text-[4rem] text-[#C4A484] font-extrabold  mb-5 '>Oops!</h1>
+        <>
+      
+
+<div className=' min-h-screen w-full'>
+        <div className='text-center flex flex-col justify-center items-center'>
+
+        <img className='w-[20rem]' src="https://img.freepik.com/free-vector/oops-404-error-with-broken-robot-concept-illustration_114360-5529.jpg" alt="" />
+           
             <h2 className='mt-4 mb-2 text-[2rem] font-semibold'>404 - Page NOT Found</h2>
             <p className='mb-6 font-bold text-gray-600'>The page you are looking for might have been removed <br /> had it's name changed or is temporary unavailable.....</p>
 
-            <Link to={"/"} className=' py-2 px-4 rounded-3xl bg-[#C4A484] text-white'>Go To HomePage</Link>
+            <Link to={"/"} className=' py-2 px-4 text-lg rounded-3xl btn text-black'>Back to home</Link>
         </div>
         
     </div>
+        </>
+       
     );
 };
 
